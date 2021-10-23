15314ea2128c1ce1da72c832b339becd6ece0b67
Lolz <58922612+aj16789@users.noreply.github.com>
2021-10-22 19:56:40 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 console.log('Hello, World!');
 
-function datentime(){
+function dateandtime(){
 	var today = new Date();
 	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 	var dateTime = date+' '+time;
 	console.log(dateTime);
 }
-setTimeout(datentime, 1000);
+dateandtime();
