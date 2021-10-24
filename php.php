c202b4904ac4707f25ca4167cf9f5e65f5a541b5
Lolz <nathan.w.waugaman@gmail.com>
2021-10-24 08:15:56 -0500

    php?

---
+++ php.php
@@ -0,0 +1,12 @@
+<?php
+include('index.html');
+?>
+
+<header>
+<div>
+<?php
+echo 'Today’s date is <b>' . date('Y/m/d') . '</b><br>!';
+echo 'Today is <b>'.date('l').'</b>!';
+?>
+</div>
+</header>
