7363971394a03140c5af83b437777d40022d121d
Lolz <nathan.w.waugaman@gmail.com>
2021-10-25 19:20:53 -0500

    Html Updates

--- php.php
+++ php.php
@@ -1,12 +1,14 @@
 <?php
-include('index.html');
+	include('index.html');
 ?>
 
-<header>
-<div>
-<?php
-echo 'Today’s date is <b>' . date('Y/m/d') . '</b><br>!';
-echo 'Today is <b>'.date('l').'</b>!';
-?>
-</div>
-</header>
+<body>
+	<header>
+		<div>
+			<?php
+				echo 'Today’s date is <b>' . date('Y/m/d') . '</b><br>!';
+				echo 'Today is <b>'.date('l').'</b>!';
+			?>
+		</div>
+	</header>
+</body>
